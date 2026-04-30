a6e210fa6f6c9f26865dfabe8f1a325aeae8eaa1
Kuba <kuba@example.com>
2026-04-30 16:26:14 +0200

    Base

---
+++ 1_Two_Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
+
+if __name__ == '__main__':
+    nums = [2,7,11,15], target = 9
+    solution = Solution()
+    assert solution(nums, target) == [0,1]
