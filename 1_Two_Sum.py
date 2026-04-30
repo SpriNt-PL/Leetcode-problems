d536ae68ca2485de74c5f7ae5229309e2b4780aa
Kuba <kuba@example.com>
2026-04-30 16:37:16 +0200

    In progress

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,8 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        for i, left_side in enumerate(nums):
+            for j, right_side in enumerate(nums):
+                if left_side + right_side == target:
+                    result = [i, j]
+                    print(result)
+                    return result 
+
 
 if __name__ == '__main__':
-    nums = [2,7,11,15], target = 9
     solution = Solution()
-    assert solution(nums, target) == [0,1]
+
+    nums = [2,7,11,15]
+    target = 9
+    assert solution.twoSum(nums, target) == [0,1]
+
+    nums = [3,2,4]
+    target = 6
+    assert solution.twoSum(nums, target) == [1,2]
