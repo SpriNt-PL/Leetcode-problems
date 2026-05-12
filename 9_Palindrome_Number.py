ad615a9ea4c40e432f1559b560d9986e829ec794
Kuba <kuba@example.com>
2026-05-12 21:32:06 +0200

    Probably solution

--- 9_Palindrome_Number.py
+++ 9_Palindrome_Number.py
@@ -1,7 +1,43 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        pass
+        x_str = str(x)
+        print(x_str)
+        left = ''
+        right = ''
+        if len(x_str) % 2 == 0:
+            for i, char in enumerate(x_str):
+                if i < len(x_str) / 2:
+                    left = ''.join([left, char])
+                else:
+                    right = ''.join([right, char])
+
+            if (left == right[::-1]):
+                return True
+            else:
+                return False
+            
+        else:
+            for i, char in enumerate(x_str):
+                if i < len(x_str) / 2 - 1:
+                    left = ''.join([left, char])
+                
+                elif i == len(x_str) // 2:
+                    continue
+                else:
+                    right = ''.join([right, char])
+
+            if (left == right[::-1]):
+                return True
+            else:
+                return False
+        
+
+                
+
+
 
 if __name__ == '__main__':
     solution = Solution()
+    assert solution.isPalindrome(1221) == True
     assert solution.isPalindrome(121) == True
+    assert solution.isPalindrome(123) == False
