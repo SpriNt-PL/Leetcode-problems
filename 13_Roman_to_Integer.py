69fafb9fdaf1b01e8fc0fc0d765a87dac2915a55
Kuba <kuba@example.com>
2026-05-12 20:12:35 +0200

    Closer to the solution

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -1,26 +1,36 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        
+        roman_numerical = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+
         number = 0
-        if len(s) == 0:
-            return number
 
         for i, char in enumerate(s):
-            if char == "I":
-                if i - 1 == len(s):
-                    if s[i + 1] == "V":
-                        number += 4
-                        continue
-                    elif s[i + 1] == "X":
-                        number += 9
-                        continue
-                number += 1
 
+            if i - 1 >= 0:
+                if roman_numerical[s[i - 1]] >= roman_numerical[s[i]]:
+                    number += roman_numerical[char]
+                else:
+                    number -= roman_numerical[char]
+            else:
+                number += roman_numerical[char]
+            
 
+        print(number)
         return number
+        
 
 
 if __name__ == '__main__':
     solution = Solution()
-    assert solution.romanToInt("III") == 3
-    assert solution.romanToInt("LVIII") == 58
+    # assert solution.romanToInt("III") == 3
+    # assert solution.romanToInt("LVIII") == 58
     assert solution.romanToInt("MCMXCIV") == 1994
