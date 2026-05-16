87bd05d636f1e6107b2856d7b82db2ff74b41703
Kuba <kuba@example.com>
2026-05-16 17:31:27 +0200

    Solution

--- 14_Longes_Common_Prefix.py
+++ 14_Longes_Common_Prefix.py
@@ -3,20 +3,31 @@ class Solution:
         common_prefix = ""
 
         shortest_str_index = 0
-        for i, str in enumerate(strs):
-            if i == 0:
-                shortest_str_index = 1
-            else:
-                if len(strs[i - 1]) > len(str):
-                    shortest_str_index = i
 
-        if len(strs[shortest_str_index]) == 0:
+        if len(strs) == 0:
             return common_prefix
+        
+        if len(strs) == 1:
+            if len(strs[0]) > 0:
+                return strs[0][0]
+            else:
+                common_prefix
+
+        for i, str in enumerate(strs):
+            
+            if len(str) > len(strs[shortest_str_index]):
+                shortest_str_index = i
 
         for i in range(len(strs[shortest_str_index])):
             char_list = []
             for str in strs:
-                char_list.append(str[i])
+                if i >= len(str):
+                    return common_prefix
+                if str != '':
+                    char_list.append(str[i])
+                    # break
+                else:
+                    return common_prefix
             
             if len(set(char_list)) == 1:
                 common_prefix = "".join([common_prefix, char_list[0]])
@@ -31,5 +42,9 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution()
-    assert s.longestCommonPrefix(strs = ["flower","flow","flight"]) == "fl"
-    assert s.longestCommonPrefix(strs = ["dog","racecar","car"]) == ""
+    # assert s.longestCommonPrefix(strs = ["flower","flow","flight"]) == "fl"
+    # assert s.longestCommonPrefix(strs = ["dog","racecar","car"]) == ""
+    # assert s.longestCommonPrefix(strs = ["","cbc","c","ca"]) == ""
+    # assert s.longestCommonPrefix(strs = ["","cbc","c","ca"]) == ""
+    # assert s.longestCommonPrefix(strs = ["cir","car"]) == "c"
+    assert s.longestCommonPrefix(strs = ["ab", "a"]) == "c"
